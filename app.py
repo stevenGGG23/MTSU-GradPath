2024c5c8601c6fe5bdfb9c0f8e5f481bb0bf223e
menaeshak331 <Mena.youssef331@gmail.com>
2026-09-24 12:07:43 -0500

    Add minors parameter to app function call

--- app.py
+++ app.py
@@ -414,7 +414,7 @@ def index():
         default_year=date.today().year,
         saved_state=saved_state,
         degree_list=degree_list,
-        minors=AVAILABLE_MINORS,  # <-- ADD THIS LINE
+        minors=AVAILABLE_MINORS,    
     )
 
 # Function that builds and displays the dependency graph
